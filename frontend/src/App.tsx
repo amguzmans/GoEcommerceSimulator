b6ac008f9b7d1125f8260bd9e481784fc563eb92
guzanam <anamargs07@gmail.com>
2026-05-07 07:26:40 -0600

    frontend config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center pt-10">
+        GoEcommerce
+      </h1>
+    </div>
+  )
+}
+
+export default App
